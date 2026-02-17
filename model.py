acb1295de078a63c7a28df6e96383e7d356f5d80
SWARNENDU95 <156882805+SWARNENDU95@users.noreply.github.com>
2026-02-17 21:53:12 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,13 @@
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
+
+MODEL_PATH = "saved_model"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def load_model():
+    tokenizer = BertTokenizer.from_pretrained(MODEL_PATH)
+    model = BertForSequenceClassification.from_pretrained(MODEL_PATH)
+    model.to(device)
+    model.eval()
+    return tokenizer, model
 
